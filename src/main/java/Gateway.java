ca5d535ca8e9d8f80675d6e65772531c85ddfd2e
cnoramut <cnoramut@amazon.com>
2025-07-21 12:08:12 -0700

    Update Gateway.java

--- src/main/java/Gateway.java
+++ src/main/java/Gateway.java
@@ -17,13 +17,12 @@ public class Gateway {
   private PPLService pplService;
   private SQLService sqlService;
   private QueryExecution queryExecution;
-  private boolean isInitialized = false;
 
   public Gateway() {
     // Empty constructor - services will be initialized when OpenSearch CLI connects
   }
 
-  public synchronized String initializeAwsConnection(String hostPort) {
+  public synchronized boolean initializeAwsConnection(String hostPort) {
     // hostPort is the AWS OpenSearch endpoint (without https://)
     Region region = new DefaultAwsRegionProviderChain().getRegion();
 
@@ -38,18 +37,17 @@ public class Gateway {
       this.sqlService = injector.getInstance(SQLService.class);
       this.queryExecution = injector.getInstance(QueryExecution.class);
 
-      this.isInitialized = true;
       System.out.println("Successfully initialized AWS connection to " + hostPort);
 
-      return "AWS Endpoint: " + hostPort + "\nRegion: " + region;
+      return true;
 
     } catch (Exception e) {
       e.printStackTrace();
-      return "initializeConnection Error: " + e;
+      return false;
     }
   }
 
-  public synchronized String initializeConnection(
+  public synchronized boolean initializeConnection(
       String host, int port, String protocol, String username, String password, boolean ignoreSSL) {
 
     try {
@@ -66,42 +64,19 @@ public class Gateway {
       this.sqlService = injector.getInstance(SQLService.class);
       this.queryExecution = injector.getInstance(QueryExecution.class);
 
-      this.isInitialized = true;
       System.out.println(
           "Successfully initialized connection to " + protocol + "://" + host + ":" + port);
-      return "Endpoints: " + host + ":" + port;
+      return true;
 
     } catch (Exception e) {
       e.printStackTrace();
-      return "initializeConnection Error: " + e;
+      return false;
     }
   }
 
-  // Defaults to PPL
-  public String queryExecution(String query) {
-    return queryExecution(query, true);
-  }
-
-  public String queryExecution(String query, boolean isPPL) {
-    return queryExecution(query, isPPL, "json");
-  }
-
   public String queryExecution(String query, boolean isPPL, String format) {
-
     // Use the QueryExecution class to execute the query
     return queryExecution.execute(query, isPPL, format);
-  }
-
-  public synchronized String disconnect() {
-
-    System.out.println("OpenSearch CLI disconnecting...");
-
-    // Reset the connection state
-    this.isInitialized = false;
-    this.pplService = null;
-    this.sqlService = null;
-
-    return "disconnect: Disconnected successfully.";
   }
 
   public static void main(String[] args) {
